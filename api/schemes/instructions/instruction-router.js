43085e95ab641ddfd0db11d284a839317c3f5b7d
mxhs <maxwellstofman@gmail.com>
2021-02-04 21:05:43 -0800

    edit

--- api/schemes/instructions/instruction-router.js
+++ api/schemes/instructions/instruction-router.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 
 router.put("/:id", async (req, res, next) => {
-  try{
-    const
-  }catch (err){
-    next(err)
-  }
-})
+	try {
+		const { id } = req.params;
+	} catch (err) {
+		next(err);
+	}
+});
 
 module.exports = router;
